dcde9637565c4cf81bc63bf020d894465edd0c1f
hatehisoka <145292892+hatehisoka@users.noreply.github.com>
2025-12-02 15:18:22 +0200

    Refactor User type to User interface with new properties

--- MedicalImageProcessor.Frontend/src/types/User/User.ts
+++ MedicalImageProcessor.Frontend/src/types/User/User.ts
@@ -1,6 +1,16 @@
-export type User = {
-  id: string;
-  email: string;
-  name: string;
-  token?: string; 
-};
+// types/User/User.ts
+export interface User {
+    id: string;
+    username: string;
+    token: string;
+}
+
+export interface UserLoginDto {
+    username: string;
+    password: string;
+}
+
+export interface UserRegisterDto {
+    username: string;
+    password: string;
+}
